f4aaed063ccdb6c6a4a350fc3c69cbb80a55b918
Yuthala <dinapolousova@gmail.com>
2025-07-13 11:37:21 +0300

    +Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,16 +5,26 @@ import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = () => {
 
-	const {user, onClose} = useTelegram();
-
-
+	// const {user, onClose} = useTelegram();
 
 	return (
 		<div className={'header'}>
-			<Button onClick={onClose}>Закрыть</Button>
+			<h1>Вас приветствует Фермерское хозяйство Green&nbsp;Pato</h1>
+			<img className="header-logo" src="img/logo.png" alt="logo" />
+			<p>Предлагаем овощи высокого качества собственного производства с бесплатной доставкой</p>
+
+			<div className="banner">
+				<ul className="banner-list">
+					<li>Бесплатная доставка по Тихорецку и Парковому</li>
+					<li>Доставка по вторникам и субботам</li>
+					<li>Наличная оплата или перевод на карту</li>
+					<li>Скидка 5% постоянным клиентам</li>
+				</ul>
+			</div>
+			{/* <Button onClick={onClose}>Закрыть</Button>
 			<span className={'username'}>
 				{user?.username}
-			</span>
+			</span> */}
 		</div>
 	);
 };
